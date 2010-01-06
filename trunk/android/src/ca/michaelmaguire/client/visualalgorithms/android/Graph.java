92b14c62dbd9c78ce6b2078aeda39c01fbd4023f
michael.maguire <michael.maguire@b49e0da8-b449-11de-8ddf-618d6b694b25>
2010-01-06 11:27:13 +0000

    Following the tutorial http://developer.android.com/guide/tutorials/hello-world.html

--- trunk/android/src/ca/michaelmaguire/client/visualalgorithms/android/Graph.java
+++ trunk/android/src/ca/michaelmaguire/client/visualalgorithms/android/Graph.java
@@ -2,12 +2,15 @@ package ca.michaelmaguire.client.visualalgorithms.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Graph extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
     }
 }
