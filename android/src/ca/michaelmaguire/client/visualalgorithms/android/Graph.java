90cb3567a413926ae983e6411912563620eb959d
michael.maguire <michael.maguire@b49e0da8-b449-11de-8ddf-618d6b694b25>
2010-01-06 11:52:36 +0000

    Working from http://android-developers.blogspot.com/2009/04/introducing-glsurfaceview.html

--- android/src/ca/michaelmaguire/client/visualalgorithms/android/Graph.java
+++ android/src/ca/michaelmaguire/client/visualalgorithms/android/Graph.java
@@ -2,15 +2,35 @@ package ca.michaelmaguire.client.visualalgorithms.android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.opengl.GLSurfaceView;
 
-public class Graph extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, Android");
-        setContentView(tv);
-    }
+public class Graph extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		// Create our Preview view and set it as the content of our Activity
+		iGLSurfaceView = new GLSurfaceView(this);
+		iGLSurfaceView.setRenderer(new CubeRenderer(false));
+		setContentView(iGLSurfaceView);
+	}
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		iGLSurfaceView.onResume();
+	}
+
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		iGLSurfaceView.onPause();
+	}
+
+	private GLSurfaceView	iGLSurfaceView;
+
 }
